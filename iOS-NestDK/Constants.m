23fcc364e600a298a20634b5521d6bc2c5a41605
Jinmenlee <jack.lee@eztable.com.tw>
2015-10-26 15:44:35 +0800

    Initial Commit

---
+++ iOS-NestDK/Constants.m
@@ -0,0 +1,34 @@
+/**
+ *  Copyright 2014 Nest Labs Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#import "Constants.h"
+
+
+// Replace this with your Nest Client ID (see https://developer.nest.com)
+NSString * const NestClientID = @"56693ba4-635d-4d6d-9794-9e580296e357";
+
+// Replace this with your Nest Client Secret (see https://developer.nest.com)
+NSString * const NestClientSecret = @"589nhEwF2hrHVOFe3dDISpKuB";
+
+NSString * const NestCurrentAPIDomain = @"home.nest.com";
+
+NSString * const NestState = @"STATE";
+
+NSString * const RedirectURL = @"http://www.guningtou.com/";
+
+NSString * const Access_Token = @"c.KYQeE4gNE23zdCDg7otXycvbTDraCZmIoRFD0N6uzx9cUSZzJcUYM4XX168aXgXAICreK2s6Cr9H1n4qRmYhTaLM4gj4Zxm5jGvfZ8R2Y1Mptmwfn0b6IxreQ3zMgFXaoy6oHfjJ1klFDvyy";
+
+
